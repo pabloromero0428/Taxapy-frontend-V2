c1a80f1e17554e8e9a333d97032f5ce44c43cc60
Cristian Vargas <cristiand.vargas@udea.edu.co>
2020-03-04 22:41:54 -0500

    Inicio de diseño del modulo de administración

--- src/app/components/administrador/administrador.component.ts
+++ src/app/components/administrador/administrador.component.ts
@@ -6,10 +6,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administrador.component.scss']
 })
 export class AdministradorComponent implements OnInit {
+  taxistas = [];
+  taxis = [];
+  
 
-  constructor() { }
+  constructor() {
+    this.taxis = [
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"},
+      {"placa": "AGCDS5A"}
+    ];
+    this.taxistas = [{
+      "idTaxista": 1124076957,
+      "nombre": "Eduardo Velasquez Velez"
+    },
+    {
+      "idTaxista": 1037667018,
+      "nombre": "Cristian David Vargas Bermúdez"
+    },
+    {
+      "idTaxista": 1128463855,
+      "nombre": "Juan Pablo Romero Laverde"
+    },{
+      "idTaxista": 1124076957,
+      "nombre": "Eduardo Velasquez Velez"
+    },
+    {
+      "idTaxista": 1037667018,
+      "nombre": "Cristian David Vargas Bermúdez"
+    },
+    {
+      "idTaxista": 1128463855,
+      "nombre": "Juan Pablo Romero Laverde"
+    },{
+      "idTaxista": 1124076957,
+      "nombre": "Eduardo Velasquez Velez"
+    },
+    {
+      "idTaxista": 1037667018,
+      "nombre": "Cristian David Vargas Bermúdez"
+    },
+    {
+      "idTaxista": 1128463855,
+      "nombre": "Juan Pablo Romero Laverde"
+    },{
+      "idTaxista": 1124076957,
+      "nombre": "Eduardo Velasquez Velez"
+    },
+    {
+      "idTaxista": 1037667018,
+      "nombre": "Cristian David Vargas Bermúdez"
+    },
+    {
+      "idTaxista": 1128463855,
+      "nombre": "Juan Pablo Romero Laverde"
+    }];
+  }
 
   ngOnInit() {
+  }
+
+  funcion() {
+    alert('hola');
+  }
+
+  infoTaxista(dni: string){
+    
   }
 
 }
